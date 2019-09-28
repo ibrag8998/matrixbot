7444402c55fb730045a0618ef84afa5c563e4c2f
ibrag8998 <ibragdzh@gmail.com>
2019-09-28 20:41:56 +0300

    I dont know wtf funcs.py wasnt working but now it works

--- funcs.py
+++ funcs.py
@@ -3,6 +3,7 @@ def determinant(mx):
     if a == 2:
         return mx[0][0]*mx[1][1]-mx[0][1]*mx[1][0]
     elif a == 3:
-        return (mx[0][0]*mx[1][1]*mx[2][2]+mx[0][1]*mx[1][2]*mx[2][0]+mx[1][0]*mx[2][1]*mx[0][2])-(mx[0][2]*mx[1][1]*mx[2][0]+mx[0][0]*mx[1][2]*mx[2][1]+mx[1][0]*mx[2][2]*mx[0][1])
+        return (mx[0][0]*mx[1][1]*mx[2][2]+mx[0][1]*mx[1][2]*mx[2][0]+mx[1][0]*mx[2][1]*mx[0][2])-\
+        (mx[0][2]*mx[1][1]*mx[2][0]+mx[0][0]*mx[1][2]*mx[2][1]+mx[1][0]*mx[2][2]*mx[0][1])
     else:
         return 'Sorry, for now I can solve only 2x2 and 3x3 determinants'
